49f79f8cc23606347bcdc06fca20e982fcfdcefb
nyilin <ningyilinnyl@gmail.com>
2022-09-26 15:12:31 +0800

    initial commit

--- tests/test_bars.R
+++
@@ -1,8 +0,0 @@
-data(df_compas_shapley_vic, package = "ShapleyVIC")
-library(magrittr)
-df_bars <- df_compas_shapley_vic %$% ShapleyVIC::summarise_shapley_vic(
-  val = shapley_vic_val, val_sd = sage_sd, var_names = var_names
-)
-df_bars %$% Figures::draw_bars(
-  val = val, var_names = Variable, x_lab = "ShapleyVIC values"
-)
